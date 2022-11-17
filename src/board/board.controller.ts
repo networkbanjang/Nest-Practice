2dc133aa3a37eae865d42f37a3019b144fdbda84
networkbanjang <tmddud73@gmail.com>
2022-11-18 02:16:21 +0900

    GET,POST

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, HttpException, UseFilters } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  UseFilters,
+  Post,
+  Body,
+} from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { Board } from './board.model';
 import { BoardService } from './board.service';
 
 @Controller('board')
@@ -7,13 +17,28 @@ export class BoardController {
   constructor(private boardService: BoardService) {}
 
   @Get('')
-  boardIndex() {
-    return this.boardService.getHi();
+  getAllBoards(): Board[] {
+    return this.boardService.getAllBoards();
   }
 
   @Get('errorTest')
   @UseFilters(HttpExceptionFilter)
-  errTest(){
-    throw new HttpException('고의적 에러',401);
+  errTest() {
+    throw new HttpException('고의적 에러', 401);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
+    return `${id}`;
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ):Board {
+
+    return this.boardService.createBoard(title,description)
   }
 }
